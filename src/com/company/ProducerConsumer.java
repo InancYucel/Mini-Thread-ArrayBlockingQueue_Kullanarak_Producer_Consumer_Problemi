22e6728367259fa2b41e1d7c6875d1ef111435fb
Inanc Yucel <inancyucel@gmail.com>
2021-12-21 13:25:31 +0300

    Initial commit

---
+++ src/com/company/ProducerConsumer.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
+public class ProducerConsumer {
+
+    Random random = new Random();
+
+    BlockingQueue<Integer> queue = new ArrayBlockingQueue<Integer>(10);
+
+    public void produce(){
+
+        while (true){
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                Integer value = random.nextInt(100);
+                queue.put(value);
+                System.out.println("Producer Üretiyor : " +value);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    public void consume(){
+
+        while(true){
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                Integer value = queue.take();
+                System.out.println("Consumer tüketiyor " + value);
+
+                System.out.println(" Queue Size: " + queue.size());
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+}
